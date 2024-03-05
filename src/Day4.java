c797f4b319e323f79fba6e4412bb6c6e3235f144
rakeshkumar1436 <sanjay.rakesh.gupta@gmail.com>
2024-03-05 21:59:54 +0530

    ..

--- src/Day4.java
+++ src/Day4.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 public class Day4 {
     public static void main(String[] args) {
-        for (int i = 1; i < 10; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
-            }
+        Scanner n =new Scanner(System.in);
+        int s = n.nextInt();
+        for (int i = 1; i <=10; i++) {
+            System.out.println(s+" x "+i+" = "+s*i);
+
         }
     }
 }
